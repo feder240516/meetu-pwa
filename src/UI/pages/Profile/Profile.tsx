c7ba1fb95f0c28d8099524df81d2aaade60313ec
Mariajose Rangel Gomez <mariajoserago@unisabana.edu.co>
2021-10-25 21:31:09 -0500

    created profile page

--- src/UI/pages/Profile/Profile.tsx
+++ src/UI/pages/Profile/Profile.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react";
 import ProfileService from "../../../Data/Services/ProfileService";
 import Card from "../../components/Card/Card";
 import React, { Component, useState } from "react";
+import SelectInput from "../../components/SelectInput/SelectInput";
 
 import "./Profile.scss";
 import { style } from "@mui/system";
@@ -21,6 +22,11 @@ export default function App() {
   const [hairColor, setHair] = useState("Blonde");
   const [title, setTitle] = useState("hola");
 
+  const state = [
+    { value: 1, label: 'Online 🟢' },
+    { value: 2, label: 'Offline 🔴' },
+  ]
+
   const checkSelection = () => {
     var imgURL = " ";
     imgURL = "/images/" + gender + skinColor + hairColor + hairStyle + ".png";
@@ -33,202 +39,34 @@ export default function App() {
     return hairIcon;
   }
   const [isChecked, setIsChecked] = useState(false);
-    const onChange = (e: any) => {
-        setIsChecked(!isChecked);
-    }
+  const onChange = (e: any) => {
+    setIsChecked(!isChecked);
+  }
   return (
     <div className="App">
-      <h1>Create your avatar!</h1>
+      <h1>Your Profile</h1>
       <img src={checkSelection()} />
-      <Card className="centered-card">
+      
+        <Card className="centered-card">
         <div className="grid">
-          <div className="radio-btn-container grid-12">
-            <div className="grid">
-              <div className="grid-4">
-                <label>Character</label>
-              </div>
-              <li className="grid-3" onClick={() => {
-                  setGender("Female");
-                }}>
-                <label className="-RadioItem">
-                  <input
-                    className="-RadioInput"
-                    type="radio"
-                    name="gender"
-                    value={gender}
-                    onChange={onChange}
-                    checked={isChecked}
-                  />
-                  <div className="-InterestItem">
-                    <div className="circle"  >
-                      <img src="/images/femenine.png" alt="" style={{ border: gender == "Female" ? "1px solid white" : "none" }}></img>
-                    </div>
-                  </div>
-                </label>
-              </li>
-              <li className="grid-3" onClick={() => {
-                  setGender("Male");
-                }}>
-                <label className="-RadioItem">
-                  <input
-                    className="-RadioInput"
-                    type="radio"
-                    name="gender"
-                    value={gender}
-                    onChange={onChange}
-                    checked={isChecked}
-                  />
-                  <div className="-InterestItem">
-                    <div className="circle"  >
-                      <img src="/images/mars.png" alt="" style={{ border: gender == "Male" ? "1px solid white" : "none" }}></img>
-                    </div>
-                  </div>
-                </label>
-              </li>
-            </div>
+          <div className="grid-12">
+            <h4>Your Name</h4>
           </div>
-        </div>
-        <div className="radio-btn-container grid-12">
-          <div className="grid">
-            <div className="grid-4">
-              <label>Hair Style</label>
-            </div>
-            <div className="radio-btn grid-3" onClick={() => {
-                  setStyle("1");
-                }}>
-                  <input
-                    type="radio"
-                    name="gender"
-                    value={gender}
-                    onChange={onChange}
-                    checked={isChecked}
-                  />
-                    <div className="circle"  >
-                      <img src={getHairIcon(1)} alt="" style={{ border: hairStyle == "1" ? "1px solid white" : "none" }} />
-                    </div>
-              </div>
-              <div className="radio-btn grid-3" onClick={() => {
-                  setStyle("2");
-                }}>
-                  <input
-                    type="radio"
-                    name="gender"
-                    value={gender}
-                    onChange={onChange}
-                    checked={isChecked}
-                  />
-                    <div className="circle"  >
-                    <img src={getHairIcon(2)} alt="" style={{ border: hairStyle == "2" ? "1px solid white" : "none" }}/>
-                    </div>
-              </div>
+          <div className="grid-12">
+            <h4>Your Career</h4>
           </div>
-          <div className="radio-btn-container grid-12">
-            <div className="grid">
-              <div className="grid-4">
-                <label>Hair Color</label>
-              </div>
-              <div
-                className="radio-btn grid-2"
-                onClick={() => {
-                  setHair("Blonde");
-                  setTitle("Blonde");
-
-                }}
-              >
-                <input
-                  type="radio"
-                  value={hairColor}
-                  name="hairColor"
-                  id="blonde"
-                  checked={hairColor == "Blonde"}
-                />
-              </div>
-              <div
-                className="radio-btn grid-2"
-                onClick={() => {
-                  setHair("Brown");
-                  setTitle("Brown");
-                }}
-              >
-                <input
-                  type="radio"
-                  value={hairColor}
-                  name="hairColor"
-                  id="brown"
-                  checked={hairColor == "Brown"}
-                />
-              </div>
-              <div
-                className="radio-btn grid-2"
-                onClick={() => {
-                  setHair("Black");
-                  setTitle("Black");
-                }}
-              >
-                <input
-                  type="radio"
-                  value={hairColor}
-                  name="hairColor"
-                  id="black"
-                  checked={hairColor == "Black"}
-                />
-              </div>
-            </div>
+          <div className="grid-12">
+            <h4>Interests</h4>
           </div>
-          <div className="radio-btn-container grid-12">
-            <div className="grid">
-              <div className="grid-4">
-                <label>Skin Color</label>
-              </div>
-              <div
-                className="radio-btn grid-2"
-                onClick={() => {
-                  setSkin("Pale");
-
-                }}
-              >
-                <input
-                  type="radio"
-                  value={skinColor}
-                  name="skinColor"
-                  id="pale"
-                  checked={skinColor == "Pale"}
-                />
-              </div>
-              <div
-                className="radio-btn grid-2"
-                onClick={() => {
-                  setSkin("Tan");
-                }}
-              >
-
-                <input
-                  type="radio"
-                  value={skinColor}
-                  name="skinColor"
-                  id="tan"
-                  checked={skinColor == "Tan"}
-                />
-              </div>
-              <div
-                className="radio-btn grid-2"
-                onClick={() => {
-                  setSkin("Dark");
-                }}
-              >
-                <input
-                  type="radio"
-                  value={skinColor}
-                  name="skinColor"
-                  id="dark"
-                  checked={skinColor == "Dark"}
-                />
-              </div>
-            </div>
+          <div className="grid-6">
+            <SelectInput
+              label="Status"
+              options={state}
+            />
           </div>
-        </div>
-      </Card>
-      <input id="next" value="NEXT" type="submit" />
+          </div>
+        </Card>
+      
     </div>
   );
 }
